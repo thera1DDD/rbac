70a9d4be698838e01a6b7ffc12c68d9f3399bc8f
thera1DDD <tagir566666@gmail.com>
2023-04-07 17:39:08 +0300

    first commit

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('title')
+403 Error
+@endsection
+@section('content')
+
+<div class="text-center">
+    <h1>У вас нет доступа к этой странице</h1>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,67 @@
+    <?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::resource('user', 'UserController');
+
+    Route::resource('permission', 'PermissionController');
+
+
+    Route::get('/profile', 'UserController@profile')->name('user.profile');
+
+    Route::post('/profile', 'UserController@postProfile')->name('user.postProfile');
+
+    Route::get('/password/change', 'UserController@getPassword')->name('userGetPassword');
+
+    Route::post('/password/change', 'UserController@postPassword')->name('userPostPassword');
+});
+
+
+Route::group(['middleware' => ['auth', 'role_or_permission:admin|create role|create permission']], function() {
+
+    Route::resource('role', 'RoleController');
+
+
+});
+
+
+
+
+
+
+
+Auth::routes();
+
+
+//////////////////////////////// axios request
+
+Route::get('/getAllPermission', 'PermissionController@getAllPermissions');
+Route::post("/postRole", "RoleController@store");
+Route::get("/getAllUsers", "UserController@getAll");
+Route::get("/getAllRoles", "RoleController@getAll");
+Route::get("/getAllPermissions", "PermissionController@getAll");
+
+/////////////axios create user
+Route::post('/account/create', 'UserController@store');
+Route::put('/account/update/{id}', 'UserController@update');
+Route::delete('/delete/user/{id}', 'UserController@delete');
+Route::get('/search/user', 'UserController@search');
